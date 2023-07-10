730c008586e6d46494abc7bfbd1c03281bc36665
Chris Tang <chtang3@illinois.edu>
2023-07-10 08:12:52 -0500

    Push initial code

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn --workers=3 app:app --timeout 1000 --log-file -
--- app.py
+++ app.py
@@ -1,164 +1,138 @@
 from flask import Flask, jsonify, render_template, request, current_app, send_file
-import base64
-from PIL import Image
+import pandas as pd
+from random_spread import generate_random_spread
 import numpy as np
 from scipy.spatial import KDTree
-import os
 
 app = Flask(__name__)
 
 # change as necessary
-tilesAcross = 200
-renderedTileSize = 70
-count = 0
+df = None
+numCorrect = 0
+selected_spread = None
+selected_spread_info = None
+   
+def generate_outright_theo(underlying, spread_type, strike, month):
+    return round(df.loc[(df['Underlying'] == underlying) & (df['Type'] == spread_type) & (df['Strike'] == strike) & (df['Month'] == month), 'TV'].values[0], 2)
+
+def generate_butterfly_theo(underlying, spread_type, low_strike, high_strike, month):
+    lower = generate_outright_theo(underlying, spread_type, low_strike, month)
+    mid = generate_outright_theo(underlying, spread_type, (high_strike + low_strike) / 2, month)
+    upper = generate_outright_theo(underlying, spread_type, high_strike, month)
+    return round((upper - mid) - (mid - lower), 2) if spread_type == 'Put' else round((lower - mid) - (mid - upper), 2)
+
+def generate_straddle_theo(underlying, strike, month):
+    call_theo = generate_outright_theo(underlying, 'Call', strike, month)
+    put_theo = generate_outright_theo(underlying, 'Put', strike, month)
+    return round(call_theo + put_theo, 2)
+
+def generate_strangle_theo(underlying, low_strike, high_strike, month, guts):
+    if guts:
+        return round(generate_outright_theo(underlying, 'Call', high_strike, month) + generate_outright_theo(underlying, 'Put', low_strike, month), 2)
+    return round(generate_outright_theo(underlying, 'Call', low_strike, month) + generate_outright_theo(underlying, 'Put', high_strike, month), 2)
 
 
-@app.route('/', methods=["GET"])
-def GET_index():
-  '''Route for "/" (frontend)'''
-  return render_template("index.html")
+def generate_call_put_spread_theo(underlying, spread_type, low_strike, high_strike, month):
+    if spread_type == 'Put':
+        return round(generate_outright_theo(underlying, spread_type, high_strike, month) - generate_outright_theo(underlying, spread_type, low_strike, month), 2)
+    return round(generate_outright_theo(underlying, spread_type, low_strike, month) - generate_outright_theo(underlying, spread_type, high_strike, month), 2)
 
-@app.route('/makeMosaic', methods=["POST"])
-def POST_makeMosaic():
-    global tilesAcross
-    global renderedTileSize
-    global count
+def generate_call_put_time_spread_theo(underlying, spread_type, strike, close_month, far_month):
+    return round(generate_outright_theo(underlying, spread_type, strike, far_month) - generate_outright_theo(underlying, spread_type, strike, close_month), 2)
 
-    # parse info from app.py
-    data = request.files["image"]
+def generate_condor_theo(underlying, spread_type, low_strike, mid_low_strike, mid_high_strike, high_strike, month):
+    wings = generate_outright_theo(underlying, spread_type, low_strike, month) + generate_outright_theo(underlying, spread_type, high_strike, month)
+    guts = generate_outright_theo(underlying, spread_type, mid_low_strike, month) + generate_outright_theo(underlying, spread_type, mid_high_strike, month)
+    return round(wings - guts, 2) if spread_type == 'Call' else round(guts - wings, 2)
 
-    # initialize base image
-    base = Image.open(data)
-    width, height = base.size
+def generate_timefly_theo(underlying, spread_type, strike, close_month, mid_month, far_month, guts_bool):
+    wings = generate_outright_theo(underlying, spread_type, strike, close_month) + generate_outright_theo(underlying, spread_type, strike, far_month)
+    guts = 2 * generate_outright_theo(underlying, spread_type, strike, mid_month)
+    return round(guts - wings, 2) if guts_bool else round(wings - guts, 2)
 
-    # determine tile sizes
-    baseTileSize = width / tilesAcross
-    baseTileFloor = int(baseTileSize)
-    baseRemainder = baseTileSize - baseTileFloor
 
-    # determine tilesPerX
-    tilesPerRow = tilesAcross
-    tilesPerCol = int (height // baseTileSize)
+def expected_theo(spread):
+    print(spread)
+    spread_underlying = spread[0]
+    spread_name = spread[1]
+    spread_month = spread[2]
+    spread_strike = spread[3]
 
-    # determine size of final mosaic
-    finalWidth = int(tilesAcross * renderedTileSize)
-    finalHeight = int(tilesPerCol * renderedTileSize)
+    if spread_name == 'c':
+        return generate_outright_theo(spread_underlying, 'Call', spread_strike, spread_month)
+    elif spread_name == 'p':
+        return generate_outright_theo(spread_underlying, 'Put', spread_strike, spread_month)
+    elif spread_name == 'cs':
+        return generate_call_put_spread_theo(spread_underlying, 'Call', spread_strike[0], spread_strike[1], spread_month)
+    elif spread_name == 'ps':
+        return generate_call_put_spread_theo(spread_underlying, 'Put', spread_strike[0], spread_strike[1], spread_month)
+    elif spread_name == 'cts':
+        return generate_call_put_time_spread_theo(spread_underlying, 'Call', spread_strike, spread_month[0], spread_month[1])
+    elif spread_name == 'pts':
+        return generate_call_put_time_spread_theo(spread_underlying, 'Put', spread_strike, spread_month[0], spread_month[1])
+    elif spread_name == 'std':
+        return generate_straddle_theo(spread_underlying, spread_strike, spread_month)
+    elif spread_name == 'stg': # assuming no gut strangle?
+        return generate_strangle_theo(spread_underlying, spread_strike[0], spread_strike[1], spread_month, True)
+    elif spread_name == 'cfly':
+        # TODO: add logic to make it so that the guts and wings are valid strike prices
+        return generate_butterfly_theo(spread_underlying, 'Call', 2000, 2200, spread_month)
+    elif spread_name == 'pfly':
+        return generate_butterfly_theo(spread_underlying, 'Put', 2000, 2200, spread_month)
+    elif spread_name == 'ctfly': # assuming no guts tfly
+        return generate_timefly_theo(spread_underlying, 'Call', spread_strike, spread_month[0], spread_month[1], spread_month[2], False)
+    elif spread_name == 'ptfly': # assuming no guts tfly
+        return generate_timefly_theo(spread_underlying, 'Put', spread_strike, spread_month[0], spread_month[1], spread_month[2], False)
+    elif spread_name == 'ccon':
+        return generate_condor_theo(spread_underlying, 'Call', spread_strike[0], spread_strike[1], spread_strike[2], spread_strike[3], spread_month)
+    elif spread_name == 'pcon':
+        return generate_condor_theo(spread_underlying, 'Put', spread_strike[0], spread_strike[1], spread_strike[2], spread_strike[3], spread_month)
+    return 0
 
-    # crop image height if necessary
-    pixels = np.array(base)
-    cropped = pixels[:tilesPerCol * baseTileFloor, :]
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  global numCorrect
+  global selected_spread
+  global selected_spread_info
 
-    # initialize sliding window
-    answer1 = Image.new('RGB', (finalWidth, finalHeight))
-    botY = 0
-    topY = baseTileFloor
-    botX = 0
-    topX = baseTileFloor
+  if request.method == 'POST':
+    print("POST")
+    try:
+        answer = float(request.form['answer'])
+    except ValueError:
+        return render_template("index.html", random_spread=selected_spread, invalid=True)
+    expected = expected_theo(selected_spread_info)
+    print('answer = ', answer, '    vs.      expected = ', expected)
+    if answer == expected:
+        numCorrect += 1
+    print("numCorrect = ", numCorrect)
+    selected_spread, selected_spread_info = generate_random_spread()
+    return render_template("index.html", random_spread=selected_spread, expected=expected, answer=answer, invalid=False)
+  else:
+    print("GET")
+    selected_spread, selected_spread_info = generate_random_spread()
+    return render_template("index.html", random_spread=selected_spread)
 
-    resBotX = 0
-    resTopX = renderedTileSize
-    resBotY = 0
-    resTopY = renderedTileSize
-
-    # go through all tiles
-    tree1 = current_app.tree1
-    map1 = current_app.map1
-    pics1 = current_app.pics1
-    for y in range(tilesPerCol):
-        botX = 0
-        topX = baseTileFloor
-        resBotX = 0
-        resTopX = renderedTileSize
-        for x in range(tilesPerRow):
-            currTile = cropped[botY:topY, botX:topX]
-            r = np.mean(currTile[:,:,0])
-            g = np.mean(currTile[:,:,1])
-            b = np.mean(currTile[:,:,2])
-            rgb = (r, g, b)
-            if y != tilesPerCol - 1 and x != tilesPerRow - 1 and baseRemainder > 0: # fraction involved
-                temp = cropped[botY:topY + 1, botX:topX + 1]
-                r2 = np.mean(temp[:,:,0])
-                g2 = np.mean(temp[:,:,1])
-                b2 = np.mean(temp[:,:,2])
-                diff1 = (r2 - r) * baseRemainder
-                diff2 = (g2 - g) * baseRemainder
-                diff3 = (b2 - b) * baseRemainder
-                rgb = (r + diff1, g + diff2, b + diff3)
-            index = tree1.query(rgb, k = 1)[1]
-            key = map1[pics1[index]]
-            region = (int(resBotX), int(resBotY), int(resTopX), int(resTopY))
-            answer1.paste(key, region)
-            botX = botX + baseTileFloor
-            topX = topX + baseTileFloor
-            resBotX = resBotX + renderedTileSize
-            resTopX = resTopX + renderedTileSize
-        botY = botY + baseTileFloor
-        topY = topY + baseTileFloor
-        resBotY = resBotY + renderedTileSize
-        resTopY = resTopY + renderedTileSize
-
-    answer1.save("Mom-Mosaic.png")
-
-    # Example - Prepare a single mosaic response:
-    res = []
-    with open("Mom-Mosaic.png", "rb") as f:
-        buffer = f.read()
-    b64 = base64.b64encode(buffer)
-    response = {
-        "image": "data:image/png;base64," + b64.decode('utf-8')
-    }
-    res.append(response)
-    count = count + 1
-    return jsonify(res), 200
+def test():
+    theo = generate_butterfly_theo(2400, 'Call', 2000, 2200, 'U')
+    print("2000 2200 c-fly TV = ", theo)
+    theo2 = generate_outright_theo(2400, 'Put', 2200, 'U')
+    print("2200 p TV = ", theo2)
+    theo3 = generate_strangle_theo(2400, 2000, 2100, 'U', True)
+    print("2000 2100 gut stg = ", theo3)
+    theo4 = generate_straddle_theo(2400, 2200, 'U')
+    print("2200 std TV = ", theo4)
+    theo5 = generate_call_put_spread_theo(2400, 'Call', 2000, 2200, 'U')
+    print("2000 2200 cs TV = ", theo5)
+    theo6 = generate_call_put_spread_theo(2400, 'Put', 2000, 2200, 'U')
+    print("2000 2200 ps TV = ", theo6)
 
 @app.before_first_request
 def initiate():
-    global renderedTileSize
-    global tilesAcross
+    global df
 
-    # process kd-trees
-    pokePics = generatePics(renderedTileSize) 
-    pics1 = []
-    map1 = {}
-    for p in pokePics:
-        curr = averageRGB(p)
-        map1[curr] = p
-        pics1.append(curr)
+    excel_file_path = 'TestTV - Copy.xlsx'
+    df = pd.read_excel(excel_file_path)
+    print(df)
 
-    # insert into kd-trees
-    tree1 = KDTree(pics1)
-    current_app.tree1 = tree1
-    current_app.pics1 = pics1
-    current_app.map1 = map1
-    return "Pics have been initialized", 200
-
-def averageRGB(image):
-  table = np.array(image)
-  r = np.mean(table[:,:,0])
-  g = np.mean(table[:,:,1])
-  b = np.mean(table[:,:,2])
-  return (r, g, b)
-
-def generatePics(ts):
-  directory = os.getcwd() + "/pictures/"
-  allPics = os.listdir(directory)
-
-  images = []
-  for pic in allPics:
-    path = os.path.join(directory, pic)
-    image = Image.open(path)
-    resized = image.resize((ts, ts))
-    resized = resized.convert("RGB")
-    images.append(resized)
-  return images
-
-@app.route('/download')
-def download():
-    if count > 0:
-        path = os.getcwd() + '/Mom-Mosaic.png'
-        print(path)
-        return send_file(path, as_attachment=True)
-    return "No mosaic has been created yet\nGo back!", 404
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True, threaded=True, timeout=300)
+    #test()
